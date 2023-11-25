424c2d861bdcfeb11dccf6c62eee12055080f12d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-11-25 22:17:00 +0200

    WIP on collision detection

---
+++ assets/start_tileset.tsx
@@ -0,0 +1,20 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="start_tileset" tilewidth="24" tileheight="24" tilecount="480" columns="20">
+ <image source="images/StarRealmsCozyForestPack24x24.png" width="480" height="576"/>
+ <tile id="65" probability="0.3"/>
+ <tile id="66" probability="0.3"/>
+ <tile id="67" probability="0.3"/>
+ <tile id="68" probability="0.3"/>
+ <tile id="69" probability="0.3"/>
+ <tile id="85" probability="0.3"/>
+ <tile id="86" probability="0.3"/>
+ <tile id="87" probability="0.3"/>
+ <tile id="88" probability="0.3"/>
+ <tile id="89" probability="0.3"/>
+ <tile id="105" probability="0.3"/>
+ <tile id="106" probability="0.3"/>
+ <tile id="107" probability="0.3"/>
+ <tile id="108" probability="0.3"/>
+ <tile id="109" probability="0.3"/>
+ <tile id="287" probability="0.6"/>
+</tileset>
