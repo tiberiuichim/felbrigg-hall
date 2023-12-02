90f0b878f114aeb2b275c3ee0cfd945e7b916381
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-12-02 22:22:09 +0200

    Add some files

---
+++ camera/camera.go
@@ -0,0 +1,30 @@
+components {
+  id: "camera"
+  component: "/orthographic/camera.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  properties {
+    id: "near_z"
+    value: "-10.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "far_z"
+    value: "10.0"
+    type: PROPERTY_TYPE_NUMBER
+  }
+  properties {
+    id: "projection"
+    value: "FIXED_ZOOM"
+    type: PROPERTY_TYPE_HASH
+  }
+}
---
+++ intro/intro.go
@@ -0,0 +1,40 @@
+components {
+  id: "introscreen"
+  component: "/intro/introscreen.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "tile_set: \"/main/main.atlas\"\n"
+  "default_animation: \"introbg\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 15.0
+    y: 15.0
+    z: 15.0
+  }
+}
