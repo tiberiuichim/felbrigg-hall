6cdcafd776fbaa41038b852e43774209165fd861
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-12-03 19:38:41 +0200

    WIP on start screen loader

---
+++ game/bootstrap.go
@@ -0,0 +1,51 @@
+components {
+  id: "bootstrap"
+  component: "/game/bootstrap.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "intro"
+  type: "collectionproxy"
+  data: "collection: \"/intro/intro.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "main"
+  type: "collectionproxy"
+  data: "collection: \"/main/main.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
