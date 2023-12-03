eb8d06f3084aa275955b612cf4bc170a7397c902
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-12-03 18:51:20 +0200

    WIP on intro screen

--- intro/intro.go
+++ intro/intro.go
@@ -16,8 +16,8 @@ components {
 embedded_components {
   id: "sprite"
   type: "sprite"
-  data: "tile_set: \"/main/main.atlas\"\n"
-  "default_animation: \"introbg\"\n"
+  data: "tile_set: \"/intro/intro.atlas\"\n"
+  "default_animation: \"introimg\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
@@ -36,5 +36,42 @@ embedded_components {
     x: 15.0
     y: 15.0
     z: 15.0
+  }
+}
+embedded_components {
+  id: "main_collection_factory"
+  type: "collectionfactory"
+  data: "prototype: \"/main/main.collection\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "main_collectionproxy"
+  type: "collectionproxy"
+  data: "collection: \"/main/main.collection\"\n"
+  "exclude: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
   }
 }
