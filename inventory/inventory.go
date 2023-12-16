ecc8421e5f0c2cc33fcf1cb64a066d363d2056fe
UltraMB <151062381+UltraMB@users.noreply.github.com>
2023-12-16 22:06:41 +0200

    add inventory
    yay

---
+++ inventory/inventory.go
@@ -0,0 +1,165 @@
+components {
+  id: "inventory"
+  component: "/inventory/inventory.gui"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "item_1"
+  type: "sprite"
+  data: "tile_set: \"/objects/objects.atlas\"\n"
+  "default_animation: \"Beginners_tome \"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 255.0
+    y: 486.0
+    z: 8.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 4.0
+    y: 4.0
+    z: 4.0
+  }
+}
+embedded_components {
+  id: "item_2"
+  type: "sprite"
+  data: "tile_set: \"/objects/objects.atlas\"\n"
+  "default_animation: \"Beginners_tome \"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 570.0
+    y: 486.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 4.0
+    y: 4.0
+    z: 1.0
+  }
+}
+embedded_components {
+  id: "item_3"
+  type: "sprite"
+  data: "tile_set: \"/objects/objects.atlas\"\n"
+  "default_animation: \"Beginners_tome \"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 879.0
+    y: 486.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 4.0
+    y: 4.0
+    z: 1.0
+  }
+}
+embedded_components {
+  id: "item_5"
+  type: "sprite"
+  data: "tile_set: \"/objects/objects.atlas\"\n"
+  "default_animation: \"Beginners_tome \"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 570.0
+    y: 254.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 4.0
+    y: 4.0
+    z: 1.0
+  }
+}
+embedded_components {
+  id: "item_6"
+  type: "sprite"
+  data: "tile_set: \"/objects/objects.atlas\"\n"
+  "default_animation: \"Beginners_tome \"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 868.0
+    y: 260.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 4.0
+    y: 4.0
+    z: 1.0
+  }
+}
+embedded_components {
+  id: "item_4"
+  type: "sprite"
+  data: "tile_set: \"/objects/objects.atlas\"\n"
+  "default_animation: \"Beginners_tome \"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  ""
+  position {
+    x: 261.0
+    y: 260.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+  scale {
+    x: 4.0
+    y: 4.0
+    z: 1.0
+  }
+}
